05d684d9ff7e6abab44218442bd247a15cf8b5b1
ricoerian <richoeriansyahm@gmail.com>
2025-02-23 18:08:24 +0700

    cek push

--- models/models.go
+++ models/models.go
@@ -6,6 +6,7 @@ import (
 	"gorm.io/gorm"
 )
 
+// User model
 type User struct {
 	gorm.Model
 	Username string    `gorm:"unique;not null"`
@@ -13,16 +14,21 @@ type User struct {
 	Messages []Message `gorm:"foreignKey:SenderID"`
 }
 
-func (m *Message) BeforeCreate(tx *gorm.DB) (err error) {
-	loc, _ := time.LoadLocation("Asia/Jakarta")
-	m.CreatedAt = time.Now().In(loc)
-	return nil
-};
-
+// Message model
 type Message struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
 	SenderID  uint      `gorm:"not null" json:"sender_id"`
 	Sender    User      `gorm:"foreignKey:SenderID"`
 	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+// BeforeCreate: Set zona waktu sebelum menyimpan ke database
+func (m *Message) BeforeCreate(tx *gorm.DB) (err error) {
+	loc, err := time.LoadLocation("Asia/Jakarta") // Pastikan zona waktu benar
+	if err != nil {
+		return err
+	}
+	m.CreatedAt = time.Now().In(loc) // Set waktu sesuai zona waktu Indonesia
+	return nil
 }
